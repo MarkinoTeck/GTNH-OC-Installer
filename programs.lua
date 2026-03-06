f9f34c6aeab29180a92e300e210debc76843fab5
Mark <93880780+MarkinoTeck@users.noreply.github.com>
2026-03-06 21:02:57 +0100

    new program

--- programs.lua
+++ programs.lua
@@ -8,6 +8,11 @@ local programs = {
     name = "AE2 & Lapotron http Tracking",
     description = "Program is designed to send ae2 and lapotronic data to a server.",
     url = "https://github.com/MarkinoTeck/GTNH-AE2-TRACKING/releases/latest/download/ComputerCode.tar"
+  },
+  {
+    name = "AE2 Crafting http Tracking",
+    description = "Program is designed to send ae2 crafting cpu data to a server.",
+    url = "https://github.com/MarkinoTeck/GTNH-AE2-CPU-TRACKING/releases/latest/download/ComputerCode.tar"
   }
 }
 
