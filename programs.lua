f74cf012ba79262178e0e683b2fb14aa9327e866
Mark <93880780+MarkinoTeck@users.noreply.github.com>
2026-03-10 21:54:58 +0100

    new program

--- programs.lua
+++ programs.lua
@@ -1,10 +1,5 @@
 local programs = {
   {
-    name = "Robot",
-    description = "Program is designed to control opencomputer robots using http requests.",
-    url = "https://github.com/MarkinoTeck/GTNH-HTTP-ROBOT-CONTROLLER/releases/latest/download/RobotCode.tar"
-  },
-  {
     name = "AE2 & Lapotron http Tracking",
     description = "Program is designed to send ae2 and lapotronic data to a server.",
     url = "https://github.com/MarkinoTeck/GTNH-AE2-TRACKING/releases/latest/download/ComputerCode.tar"
@@ -13,6 +8,16 @@ local programs = {
     name = "AE2 Crafting http Tracking",
     description = "Program is designed to send ae2 crafting cpu data to a server.",
     url = "https://github.com/MarkinoTeck/GTNH-AE2-CPU-TRACKING/releases/latest/download/ComputerCode.tar"
+  },
+  {
+    name = "Robot",
+    description = "Program is designed to control opencomputer robots using http requests.",
+    url = "https://github.com/MarkinoTeck/GTNH-HTTP-ROBOT-CONTROLLER/releases/latest/download/RobotCode.tar"
+  },
+  {
+    name = "Adapter Controller",
+    description = "Program is designed set filters in ae2 components using adapters.",
+    url = "https://github.com/MarkinoTeck/GTNH-HTTP-ADAPTER-CONTROLLER/releases/latest/download/ServerCode.tar"
   }
 }
 
